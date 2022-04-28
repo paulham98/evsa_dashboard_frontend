6dedbec7b31025785fa1808c1d846799874f3159
paulham98 <pauljungho.ham@gmail.com>
2022-04-28 16:42:28 +0900

    접수대수,잔여대수 바 차트 추가

--- vue_files/src/components/Main.vue
+++ vue_files/src/components/Main.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="hello">
     <Top_navBar></Top_navBar>
-    <router-link  :to="{name:'Ex_router'}">Router Example</router-link>
+    <apexchart class="bar1" type="bar" width="300" height="300" :options="bar1_chartOptions" :series="bar1_series"></apexchart>
+    <apexchart class="bar2" type="bar" width="300" height="300" :options="bar2_chartOptions" :series="bar2_series"></apexchart>
     <ApexCharts></ApexCharts>
+    <router-link  :to="{name:'Ex_router'}">Router Example</router-link>
   </div>
 </template>
 
@@ -15,6 +17,108 @@ export default {
   components: {
     Top_navBar,
     ApexCharts
+  },
+  data(){
+    return{
+      bar1_series: [{
+        name: 'Marine Sprite',
+        data: [44,]
+      }, {
+        name: 'Striking Calf',
+        data: [53,]
+      }
+      ],
+      bar1_chartOptions: {
+        chart: {
+          type: 'bar',
+          height: 350,
+          stacked: true,
+          stackType: '100%',
+          width: 200
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true,
+            borderRadius:5
+          },
+        },
+        stroke: {
+          width: 1,
+          colors: ['#fff']
+        },
+        title: {
+          text: '100% Stacked Bar'
+        },
+        xaxis: {
+          categories: [2008, 2009, 2010, 2011, 2012, 2013, 2014],
+        },
+        tooltip: {
+          y: {
+            formatter: function (val) {
+              return val + "K"
+            }
+          }
+        },
+        fill: {
+          opacity: 1
+
+        },
+        legend: {
+          position: 'top',
+          horizontalAlign: 'left',
+          offsetX: 40
+        }
+      },
+      bar2_series: [{
+        name: 'Marine Sprite',
+        data: [44,]
+      }, {
+        name: 'Striking Calf',
+        data: [53,]
+      }
+      ],
+      bar2_chartOptions: {
+        chart: {
+          type: 'bar',
+          height: 350,
+          stacked: true,
+          stackType: '100%',
+          width: 200
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true,
+            borderRadius:5
+          },
+        },
+        stroke: {
+          width: 1,
+          colors: ['#fff']
+        },
+        title: {
+          text: '100% Stacked Bar'
+        },
+        xaxis: {
+          categories: [2008, 2009, 2010, 2011, 2012, 2013, 2014],
+        },
+        tooltip: {
+          y: {
+            formatter: function (val) {
+              return val + "K"
+            }
+          }
+        },
+        fill: {
+          opacity: 1
+
+        },
+        legend: {
+          position: 'top',
+          horizontalAlign: 'left',
+          offsetX: 40
+        }
+      }
+    }
   }
 }
 </script>
