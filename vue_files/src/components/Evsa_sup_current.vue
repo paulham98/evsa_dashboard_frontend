536cd7dfaefbaf1579efcbba9fc8c4543fb047f3
paulham98 <pauljungho.ham@gmail.com>
2022-04-30 17:33:08 +0900

    text 나타내기 효과 추가 및 text rolling 작업중

--- vue_files/src/components/Evsa_sup_current.vue
+++ vue_files/src/components/Evsa_sup_current.vue
@@ -1,5 +1,26 @@
 <template>
   <Top_navBar></Top_navBar>
+  <select v-model="sup_av" @change="select_sup_ab(0, this.sup_show)" >
+    <option  v-for="(v, i) in sup_availability" :value="v" :key="i+1">{{v}}</option>
+  </select>
+  <select v-model="deadline_av" @change="select_sup_ab(1, this.deadline_show, deadline_av)">
+    <option v-for="(v, i) in deadline" :value="v" :key="i">{{v}}</option>
+  </select>
+  <!--
+  <transition>
+    <p >지원금 자격 부여 가능성이<span>{{sup_av}}</span>입니다.</p>
+  </transition>
+  -->
+  <Transition name="fadeOne">
+    <p >지원금 자격 부여 가능성이{{sup_av}}입니다.</p>
+  </Transition>
+  <transition name="fadeOne">
+    <p v-show="!sup_show" class="container">지원금 자격 부여 가능성이 {{sup_av}}입니다.</p>
+  </transition>
+  <Transition name="fadeTwo" mode="out-in">
+    <p v-if="deadline_show" :class="{deadline_red:isRed, dealine_black:isBlack}">{{deadline_av}}</p>
+    <p v-else :class="{deadline_red:isRed}">{{deadline_av}}</p>
+  </Transition>
   <apexchart class="bar1" type="bar" width="300" height="300" :options="bar1_chartOptions" :series="bar1_series"></apexchart>
   <apexchart class="bar2" type="bar" width="300" height="300" :options="bar2_chartOptions" :series="bar2_series"></apexchart>
   <ApexCharts></ApexCharts>
@@ -16,6 +37,14 @@
     },
     data(){
       return{
+        sup_availability: ['매우 낮은편', '낮은편', '보통', '높은편', '매우 높은편'],
+        deadline: ['마감여유','마감임박', '마감위험'],
+        deadline_av: '마감여유',
+        isRed:false,
+        isBlack: true,
+        sup_show:true,
+        deadline_show: true,
+        sup_av: '매우 낮은편',
         bar1_series: [{
           name: 'Marine Sprite',
           data: [44,]
@@ -115,10 +144,47 @@
           }
         }
       }
-    }
+    },
+    methods:{
+      select_sup_ab(i, show, deadline){
+        if(show && i === 0){
+          this.sup_show=false
+        }else if(!show && i === 0){
+          this.sup_show=true
+        }
+        if(show && i === 1 ){
+          this.deadline_show = false
+        }else if(!show && i === 1){
+          this.deadline_show = true
+        }
+        if( i === 1 && deadline === '마감임박'){
+          this.isRed = true;
+          this.isBlack = false
+        }else if( i === 1 && deadline === '마감위험'){
+          this.isRed = true;
+          this.isBlack = false
+        }else if( i === 1 && deadline === '마감여유'){
+          this.isRed = false;
+          this.isBlack = true
+        }
+      }
+    },
   }
 </script>
 
 <style scoped>
-
+  .deadline_red{
+    color:red
+  }
+  .deadline_black{
+    color: black
+  }
+  .container {
+    clip-path: polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%);
+  }
+  .fadeOne-enter-active, .fadeOne-leave-active{transition: all .5s ease-out; }
+  .fadeOne-enter, .fadeOne-leave-to{clip-path: polygon(0% 100%, 100% 100%, 0% 100%, 0% 100%);transform: translate(0%, -100%);}
+  .fadeOne-enter-from, fadeOne-leave-to{clip-path: polygon(0% 100%, 100% 100%, 0% 100%, 0% 100%);transform: translate(0%, 100%);}
+  .fadeTwo-enter-active, .fadeTwo-leave-active{transition: opacity 1s ease;}
+  .fadeTwo-enter-from, .fadeTwo-leave-to{opacity: 0;}
 </style>
