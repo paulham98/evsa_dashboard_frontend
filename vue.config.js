7de5d4c171599ab92af3628925fc2bb20217dcae
Kyeongrok Kim <oceanfog1@gmail.com>
2022-06-30 13:03:27 +0900

    env설정

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,23 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 });
+const proxy = {
+  '/auth': {
+    target: `${process.env.VUE_APP_AUTH_BASE_URL ?? process.env.VUE_APP_BASE_URL}`
+  },
+  '/order': {
+    target: `${process.env.VUE_APP_ORDER_BASE_URL ?? process.env.VUE_APP_BASE_URL}`
+  },
+  '/product': {
+    target: `${process.env.VUE_APP_PRODUCT_BASE_URL ?? process.env.VUE_APP_BASE_URL}`
+  },
+  '/shipment': {
+    target: `${process.env.VUE_APP_SHIPMENT_BASE_URL ?? process.env.VUE_APP_BASE_URL}`
+  },
+}
+
+console.log(proxy)
+
 module.exports = {
   devServer: {
     proxy: `http://15.165.32.56:30423`
